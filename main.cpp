a8fa9778f7aad440968ac627caba7955f288c6b9
ThatCodingFrog <aiden.schuneman@outlook.com>
2025-11-06 11:02:53 -0700

    I'm going to restart. Push

---
+++ main.cpp
@@ -0,0 +1,56 @@
+/*main file.  Program starts and ends here*/
+
+//this current main was made by ChatGPT, as a launching point for me
+
+// main.cpp
+#include <SDL.h>
+#include <glad/glad.h>
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_opengl3.h"
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Harbour",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 150");
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT) running = false;
+        }
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame(window);
+        ImGui::NewFrame();
+
+        // --- Your first UI ---
+        ImGui::Begin("Harbour Test");
+        ImGui::Text("If you can see this, Dear ImGui works!");
+        ImGui::End();
+
+        ImGui::Render();
+        glViewport(0, 0, 1280, 720);
+        glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
---
+++ setup-env.bat
@@ -0,0 +1,3 @@
+@echo off
+set PATH=C:\Users\<your_name>\AppData\Local\Programs\Git\cmd;%PATH%
+cmd
